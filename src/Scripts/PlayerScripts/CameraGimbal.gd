51c8637b85cc4758570aa404db8733bd7f957f89
alizardguy <coyotehelp123@gmail.com>
2023-04-11 12:03:21 -0700

    made camera sorta function

--- src/Scripts/PlayerScripts/CameraGimbal.gd
+++ src/Scripts/PlayerScripts/CameraGimbal.gd
@@ -10,12 +10,9 @@ func _process(delta):
 	if (Input.is_action_just_pressed("camera_drag_click")):
 		rotating = true;
 		prev_mouse_position = get_viewport().get_mouse_position()
-	if (Input.is_action_just_pressed("camera_drag_click")):
+	if (Input.is_action_just_released("camera_drag_click")):
 		rotating = false;
-	
-	if (rotating):
-		print("rotating..")
+	if rotating:
 		next_mouse_position = get_viewport().get_mouse_position()
-		rotate_y((next_mouse_position.x - prev_mouse_position.x) * .1 * delta)
-		rotate_z(-(next_mouse_position.y - prev_mouse_position.y) * .1 * delta)
+		rotate_x((next_mouse_position.y - prev_mouse_position.y) * .1 * delta)
 		prev_mouse_position = next_mouse_position
