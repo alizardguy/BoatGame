9aa0f22950a7af075f8ca71f540cea72f280a9af
alizard <coyotehelp123@gmail.com>
2023-04-14 09:52:36 -0700

    Added fps counter
    Added fps counter and made some code more consistent in format.

--- src/Scripts/Interface/MainUI/user_interface.gd
+++ src/Scripts/Interface/MainUI/user_interface.gd
@@ -2,14 +2,16 @@ extends Control
 
 var player_vars;
 var hp_display;
+var fps_display;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	player_vars = get_node("/root/PlayerVariables");
 	hp_display = get_node("hp_debug");
+	fps_display = get_node("fps_debug");
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	hp_display.text = str(player_vars.playerHealth) + "/" + str(player_vars.maxHealth) + " HP";
-	pass
+	fps_display.text = str(Engine.get_frames_per_second()) + " FPS";
--- src/Scripts/PlayerScripts/Player.gd
+++ src/Scripts/PlayerScripts/Player.gd
@@ -15,21 +15,21 @@ func _ready():
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
-		velocity.y -= gravity * delta
+		velocity.y -= gravity * delta;
 
 	# Handle Jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		velocity.y = JUMP_VELOCITY;
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("move_left", "move_right", "move_up", "move_down")
-	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var input_dir = Input.get_vector("move_left", "move_right", "move_up", "move_down");
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized();
 	if direction:
-		velocity.x = direction.x * currentSpeed
-		velocity.z = direction.z * currentSpeed
+		velocity.x = direction.x * currentSpeed;
+		velocity.z = direction.z * currentSpeed;
 	else:
 		velocity.x = move_toward(velocity.x, 0, currentSpeed);
 		velocity.z = move_toward(velocity.z, 0, currentSpeed);
 
-	move_and_slide()
+	move_and_slide();
