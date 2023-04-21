9463c084e2d91e138f9f67d5c363759c508d0877
alizardguy <coyotehelp123@gmail.com>
2023-04-21 16:42:28 -0700

    Update collectible_item_base.gd

--- src/Scripts/Items/collectible_item_base.gd
+++ src/Scripts/Items/collectible_item_base.gd
@@ -4,6 +4,10 @@ class_name CollectibleItemBase
 
 extends Node3D
 
+# position item started in
+@onready var default_pos = get_position();
+
+# idk yet
 var collect_value: float;
 var collect_type: String;
 
@@ -13,3 +17,6 @@ func collected():
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	print("collectible in scene");
+
+func _process(_delta):
+	pass # make float up and down effect
